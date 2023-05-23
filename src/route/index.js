037b1e1d1c044123af478d816ac371d7e359ee17
unknown <n.larivanko@gmail.com>
2023-05-23 13:28:34 +0300

    css lesons

--- src/route/index.js
+++ src/route/index.js
@@ -741,6 +741,18 @@ router.get('/template-5', function (req, res) {
       {
         title: "Loyalty Program",
         text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
+      },
+      {
+        title: "Loyalty Program",
+        text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
+      },
+      {
+        title: "Loyalty Program",
+        text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
+      },
+      {
+        title: "Loyalty Program",
+        text: "While you were paying, we had already prepared access to our Member Area, where you can read more about the status of your order."
       }
     ],
 
